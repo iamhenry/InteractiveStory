41c88287120d0aa36c2454a987f70adef534ac27
henry moran <virtualhenry@gmail.com>
2017-12-06 16:45:21 -0800

    Added anchors via code for image views

--- InteractiveStory/InteractiveStory/PageController.swift
+++ InteractiveStory/InteractiveStory/PageController.swift
@@ -12,6 +12,13 @@ class PageController: UIViewController {
     
     var page: Page?
     
+    // MARK: - User Interface Properties
+    
+    let artworkView = UIImageView()
+    let storyLabel = UILabel()
+    let firstChoiceButton = UIButton(type: .system)
+    let secondChoiceButton = UIButton(type: .system)
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
@@ -25,13 +32,32 @@ class PageController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        view.backgroundColor = .blue
+        
+        if let page = page {
+            artworkView.image = page.story.artwork
+
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        view.addSubview(artworkView)
+        artworkView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            artworkView.topAnchor.constraint(equalTo: view.topAnchor),
+            artworkView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            artworkView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            artworkView.leftAnchor.constraint(equalTo: view.leftAnchor)
+        ])
     }
  
 
