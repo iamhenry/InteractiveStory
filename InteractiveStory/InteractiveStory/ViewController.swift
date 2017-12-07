5fcd0645d48749a65d2285c91034556bbf039ddb
henry moran <virtualhenry@gmail.com>
2017-12-06 22:27:28 -0800

    added alert view for when user doesn't type in their name

--- InteractiveStory/InteractiveStory/ViewController.swift
+++ InteractiveStory/InteractiveStory/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,10 +24,28 @@ class ViewController: UIViewController {
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "startAdventure" {
-            guard let pageController = segue.destination as? PageController else {
-                return
+            
+                do {
+                    if let name = nameTextField.text {
+                        if name == "" {
+                            throw AdventureError.nameNotProvided
+                        } else {
+                            guard let pageController = segue.destination as? PageController else {
+                                return
+                            }
+                            pageController.page = Adventure.story(withName: "Henry")
+                        }
+                        }
+                } catch AdventureError.nameNotProvided {
+                    let alertController = UIAlertController(title: "Name Not Provided", message: "Provide a name to start the story", preferredStyle: .alert)
+                    
+                    let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alertController.addAction(action)
+                    
+                    present(alertController, animated: true, completion: nil)
+                } catch let error {
+                    fatalError("\(error.localizedDescription)")
             }
-            pageController.page = Adventure.story
         }
     }
     
